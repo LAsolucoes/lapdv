c82430be711e226ff8aa9ea0c804b45f406afab6
Alex Moedo <alexmoedo@gmail.com>
2024-12-16 11:50:55 -0300

    ajustes

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,10 +1,10 @@
-/* FUNÇAÕ QUE ABRE E FECHA OS SUBMENU DO SIDEBAR */
- function openCloseMenuSidebar(menuId) {
+/* FUNÇÃO QUE ABRE E FECHA OS SUBMENU DO SIDEBAR */
+function openCloseMenuSidebar(menuId) {
   const subMenuSidebar = document.getElementById(menuId);
-  subMenuSidebar.classList.toggle("hidden");
+  if (subMenuSidebar) {
+    subMenuSidebar.classList.toggle("hidden");
+  }
 }
-
-
 
 // Função para carregar o sidebar dinamicamente
 document.addEventListener('DOMContentLoaded', function() {
@@ -12,29 +12,30 @@ document.addEventListener('DOMContentLoaded', function() {
   if (window.location.pathname !== '/pdv.html') {
     const sidebarContainer = document.querySelector('#sidebar-container');
 
-    // Carregar o conteúdo do sidebar.html
-    fetch('sidebar.html') // Caminho relativo ao arquivo do sidebar
-      .then(response => response.text())
-      .then(data => {
-        sidebarContainer.innerHTML = data;
+    // Verificar se o #sidebar-container existe no DOM
+    if (sidebarContainer) {
+      // Carregar o conteúdo do sidebar.html
+      fetch('/templates/sidebar.html') // Caminho relativo ao arquivo do sidebar
+        .then(response => response.text())
+        .then(data => {
+          // Definir o conteúdo de #sidebar-container com o conteúdo carregado
+          sidebarContainer.innerHTML = data;
 
-        // Após o sidebar ser carregado, adicionar os event listeners
-        
-document.getElementById("menu_cadastros").addEventListener("click" , ()=> openCloseMenuSidebar('cadastros'))
- document.getElementById("menu_estoque").addEventListener("click" , () => openCloseMenuSidebar("estoque"))
- document.getElementById("menu_vendas").addEventListener("click" , () => openCloseMenuSidebar("vendas"))
- document.getElementById("menu_financeiro").addEventListener("click" , () => openCloseMenuSidebar("financeiro"))
- document.getElementById("menu_mesas").addEventListener("click" , () => openCloseMenuSidebar("mesas"))
- document.getElementById("menu_comandas").addEventListener("click" , () => openCloseMenuSidebar("comandas"))
- document.getElementById("menu_relatorios").addEventListener("click" , () => openCloseMenuSidebar("relatorios"))
- document.getElementById("menu_configuracoes").addEventListener("click" , () => openCloseMenuSidebar("configuracoes"))
-      })
-      
-      
+          // Após o sidebar ser carregado, adicionar os event listeners
+          document.getElementById("menu_cadastros")?.addEventListener("click", () => openCloseMenuSidebar('cadastros'));
+          document.getElementById("menu_estoque")?.addEventListener("click", () => openCloseMenuSidebar("estoque"));
+          document.getElementById("menu_vendas")?.addEventListener("click", () => openCloseMenuSidebar("vendas"));
+          document.getElementById("menu_financeiro")?.addEventListener("click", () => openCloseMenuSidebar("financeiro"));
+          document.getElementById("menu_mesas")?.addEventListener("click", () => openCloseMenuSidebar("mesas"));
+          document.getElementById("menu_comandas")?.addEventListener("click", () => openCloseMenuSidebar("comandas"));
+          document.getElementById("menu_relatorios")?.addEventListener("click", () => openCloseMenuSidebar("relatorios"));
+          document.getElementById("menu_configuracoes")?.addEventListener("click", () => openCloseMenuSidebar("configuracoes"));
+        })
+        .catch(error => {
+          console.error("Erro ao carregar o conteúdo do sidebar:", error);
+        });
+    } else {
+      console.error("#sidebar-container não encontrado no DOM.");
+    }
   }
 });
-
-
-
-
-
